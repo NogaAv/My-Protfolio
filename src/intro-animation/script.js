685f726275841a0ae414322725b824c4c0937bc8
Noga Avraham <37444713+NogaAv@users.noreply.github.com>
2025-01-04 18:36:14 +0200

    Update script.js

--- src/intro-animation/script.js
+++ src/intro-animation/script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = (canvas.width = 2700); //we give the same value as in the styles.css file
 const CANVAS_HEIGHT = (canvas.height = 700);
-const layersPath = 'src/intro-animation/backgroundLayers';
+const layersPath = './src/intro-animation/backgroundLayers';
 let gameSpeed = 3;
 
 const backgroundLayer1 = new Image(); //exactly like: document.createElement("img");
